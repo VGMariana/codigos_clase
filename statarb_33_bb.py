1ace997a1967abf2fd817a1ea0fe4075dab02594
Finanzas Cuantitativas FC <69920097+finanzascuantitativasfc@users.noreply.github.com>
2021-01-11 12:01:41 -0600

    Add files via upload

--- statarb_33_bb.py
+++ statarb_33_bb.py
@@ -31,7 +31,7 @@ backtest.ric_short = 'REP.MC'
 backtest.rolling_days = 20
 backtest.level_1 = 1.
 backtest.level_2 = 2.
-backtest.data_cut = 0.1
+backtest.data_cut = 0.7
 backtest.data_type = 'in-sample' # in-sample out-of-sample
 
 # load data
@@ -42,12 +42,10 @@ if backtest.data_type == 'in-sample':
 elif backtest.data_type == 'out-of-sample':
     df1 = t.tail(t.shape[0]-cut)
 df1 = df1.reset_index(drop=True)
-
 # spread at current close
 df1['spread'] = df1['price_1']/df1['price_2']
 base = df1['spread'][0]
 df1['spread'] = df1['spread'] / base
-
 # spread at previous close
 df1['spread_previous'] = df1['price_1_previous']/df1['price_2_previous']
 df1['spread_previous'] = df1['spread_previous'] / base
@@ -165,7 +163,6 @@ for n in range(size):
             can_trade = False
         else:
             exit_signal = 0
-        
     # save data for the day
     m = 0
     mtx_backtest[n][m] = position
@@ -179,13 +176,11 @@ for n in range(size):
     mtx_backtest[n][m] = trade
     m = m + 1
     mtx_backtest[n][m] = pnl_trade
-    
 df2 = pd.DataFrame(data=mtx_backtest,columns=columns)
 df = pd.concat([timeseries,df2], axis=1) # axis=0 for rows, axis=1 for columns
 df = df.dropna()
 df = df.reset_index(drop=True)
 df['cum_pnl_daily'] = np.cumsum(df['pnl_daily'])
-
 # compute Sharpe ratio and number of trades
 vec_pnl = df['pnl_daily'].values
 pnl_mean = np.round(np.mean(vec_pnl) * 252, 4)
@@ -194,7 +189,7 @@ sharpe = np.round(pnl_mean / pnl_volatility, 4)
 df3 = df[df['trade'] == 1]
 nb_trades = df3.shape[0]
 
-# plot cum pnl
+# plot cum pnl of the trading strategy
 plot_str = 'cumulative PNL daily ' + str(backtest.ric_long) + ' / ' + str(backtest.ric_short) + '\n'\
     + 'pnl annual mean ' + str(pnl_mean) + '\n'\
     + 'pnl annual volatility ' + str(pnl_volatility) + '\n'\
---
+++ statarb_34_bb.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 09:14:21 2021
+
+@author: Meva
+"""
+
+# import libraries and functions
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import scipy
+import importlib
+import matplotlib.pyplot as plt
+from scipy.stats import skew, kurtosis, chi2, linregress
+from scipy.optimize import minimize
+from numpy import linalg as LA
+
+# import our own files and reload
+import stream_functions
+importlib.reload(stream_functions)
+import stream_classes
+importlib.reload(stream_classes)
+import bollinger_bands
+importlib.reload(bollinger_bands)
+
+# inputs
+backtest = bollinger_bands.backtest()
+backtest.ric_long = 'TOTF.PA'
+backtest.ric_short = 'REP.MC'
+backtest.rolling_days = 20
+backtest.level_1 = 0.8
+backtest.level_2 = 1.6
+backtest.data_cut = 0.7
+backtest.data_type = 'in-sample' # in-sample out-of-sample
+
+
+# load data
+backtest.load_data()
+
+# compute indicator
+backtest.compute_indicators(bool_plot=True)
+
+# run backtest of trading strategy
+backtest.run_strategy(bool_plot=True)
+
+# get results in a dataframe format
+df_strategy = backtest.dataframe_strategy
